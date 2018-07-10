9b9ed114f398f4e5d1e7b85cec0520c6cfc351bc
Dave Chen <zhenyu.chen@campus.lmu.de>
2018-07-10 09:56:17 +0200

    changed batch size

--- main.sh
+++ main.sh
@@ -2,11 +2,11 @@ python train.py \
 --train_size=-1 \
 --val_size=-1 \
 --test=-1 \
---epoch=20 \
+--epoch=50 \
 --verbose=200 \
 --learning_rate=5e-4 \
 --weight_decay=1e-4 \
---batch_size=512 \
+--batch_size=64 \
 --gpu=3 \
 --pretrained=resnet \
 --attention=spatial \
